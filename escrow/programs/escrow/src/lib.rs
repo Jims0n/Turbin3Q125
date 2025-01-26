140991f7403ce601e5c5c65c9b41c4d65ad7a9a2
Jimson <opeyemiabdulateef406@gmail.com>
2025-01-26 08:27:48 +0100

    add escrow

---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+use crate::instructions::*;
+
+declare_id!("A2rhTqfm2uHyChiDurS66FUV42fURpkWHeGus7qRQDLq");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive_amount: u64, deposit_amount: u64,) -> Result<()> {
+       ctx.accounts.init_escrow_state(seed, receive_amount, ctx.bumps)?;
+       ctx.accounts.deposit(deposit_amount)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.transfer_to_maker()?;
+        ctx.accounts.transfer_to_taker()?;
+        ctx.accounts.close_vault()?;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund()?;
+        ctx.accounts.close()?;
+        Ok(())
+    }
+}
+
